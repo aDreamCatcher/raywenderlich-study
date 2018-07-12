f553fc0807e98ddc63014a2a5cf5601e2df4971a
xin <xin@Macmini.local>
2018-07-12 18:43:00 +0800

    part

---
+++ iOS/Core Text Tutorial/CoreTextMagazine/CoreTextMagazine/CTColumnView.swift
@@ -0,0 +1,49 @@
+//
+//  CTColumnView.swift
+//  CoreTextMagazine
+//
+//  Created by xin on 2018/7/12.
+//  Copyright © 2018年 xin. All rights reserved.
+//
+
+import UIKit
+
+class CTColumnView: UIView {
+
+    // MARK: - Properties
+    var ctFrame: CTFrame!
+    var images: [(image: UIImage, frame: CGRect)] = []
+    
+    // MARK: - Initializers
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+    
+    required init(frame: CGRect, ctFrame: CTFrame) {
+        super.init(frame: frame)
+        self.ctFrame = ctFrame
+        backgroundColor = .white
+    }
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
+        context.textMatrix = .identity
+        context.translateBy(x: 0, y: bounds.size.height)
+        context.scaleBy(x: 1.0, y: -1.0);
+        
+        CTFrameDraw(ctFrame, context)
+        
+        // draw image
+        for imageData in images {
+            if let image = imageData.image.cgImage {
+                let imageBounds = imageData.frame
+                context.draw(imageData, in: imageBounds)
+            }
+        }
+    }
+ 
+
+}
--- iOS/Core Text Tutorial/CoreTextMagazine/CoreTextMagazine/CTView.swift
+++ iOS/Core Text Tutorial/CoreTextMagazine/CoreTextMagazine/CTView.swift
@@ -9,27 +9,66 @@
 import UIKit
 import CoreText
 
-class CTView: UIView {
-
+class CTView: UIScrollView {
     
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
+    // MARK: - Properties
+    var imageIndex: Int!
+    
+    // 1
+    func buildFrames(withAttrString attrString: NSAttributedString,
+                     andImages images: [[String: Any]]) {
         // 2
-        guard let context = UIGraphicsGetCurrentContext() else { return }
+        isPagingEnabled = true
         // 3
-        let path = CGMutablePath()
-        path.addRect(bounds)
+        let frameSetter = CTFramesetterCreateWithAttributedString(attrString as CFAttributedString)
         // 4
-        let attrString = NSAttributedString(string: "Hello World")
+        var pageView = UIView()
+        var textPos = 0
+        var columnIndex: CGFloat = 0
+        var pageIndex: CGFloat = 0
+        let settings = CTSettings()
         // 5
-        let framesetter = CTFramesetterCreateWithAttributedString(attrString)
-        // 6
-        let frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, attrString.length), path, nil)
-        // 7
-        CTFrameDraw(frame, context)
+        while textPos < attrString.length {
+            if columnIndex.truncatingRemainder(dividingBy: settings.columnsPerPage) == 0{
+                columnIndex = 0
+                pageView = UIView(frame: settings.pageRect.offsetBy(dx: pageIndex * bounds.width, dy: 0))
+                addSubview(pageView)
+                // 2
+                pageIndex += 1
+            }
+            // 3
+            let columnXOrigin = pageView.frame.size.width / settings.columnsPerPage
+            let columnOffset = columnIndex * columnXOrigin
+            let columnFrame = settings.columnRect.offsetBy(dx: columnOffset, dy: 0)
+            
+            // 1
+            let path = CGMutablePath()
+            path.addRect(CGRect(origin: .zero, size: columnFrame.size))
+            let ctFrame = CTFramesetterCreateFrame(frameSetter, CFRangeMake(textPos, 0), path, nil)
+            // 2
+            let column = CTColumnView(frame: columnFrame, ctFrame: ctFrame)
+            pageView.addSubview(column)
+            // 3
+            let frameRange = CTFrameGetVisibleStringRange(ctFrame)
+            textPos += frameRange.length
+            // 4
+            columnIndex += 1
+            
+            contentSize = CGSize(width: CGFloat(pageIndex) * bounds.width,
+                                 height: bounds.height)
+        }
         
     }
     
-
+    //
+    func attachImagesWithFrame(_ images: [[String: Any]],
+                               ctFrame: CTFrame,
+                               margin: CGFloat,
+                               columnView: CTColumnView) {
+        // 1
+        let lines = CTFrameGetLines(ctFrame) as NSArray
+        
+        <#function body#>
+    }
+    
 }
